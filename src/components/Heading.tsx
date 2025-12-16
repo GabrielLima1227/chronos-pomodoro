9ebae823345f57e14feba56d5f72bb5f03edc500
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-12-16 11:12:04 -0300

    feat: add initial React component, global CSS, and css models

---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import style from "./Heading.module.css";
+
+export function Heading() {
+    return (
+        <>
+            <h1 className={style.heading}>Welcome to Chronos Pomodoro!</h1>
+        </>
+    )
+}
