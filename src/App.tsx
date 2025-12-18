b35bd32ca88b2c1e82924581448ffae85883ae6d
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-12-17 22:26:22 -0300

    feat: implementar componentes Logo e Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { Container } from './components/Container';
+
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
-import { Container } from './components/Container';
 
 export function App() {
     return (
         <>
             <Container>
-                <Heading>Logo</Heading>
+                <Logo />
             </Container>
             <Container>
-                <Heading>Menu</Heading>
+                <Menu />
             </Container>
         </>
     );
