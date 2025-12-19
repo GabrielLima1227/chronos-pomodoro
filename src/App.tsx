ee0f5f5a2b9ed1e62a7defe13cae001f1fbc7140
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-12-18 23:14:08 -0300

    feat: criado componentes Cycles & DefaultButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { Cycles } from './components/Cycles';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/Defaultinput';
+import { DefaultButton } from './components/DefaultButton';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -23,17 +26,17 @@ export function App() {
             <Container>
                 <form className='form' action="">
                     <div className="formRow">
-                        <DefaultInput id='meuInput' type="number" placeholder="Digite sua tarefa" labelText='task' title='sdasd'/>
+                        <DefaultInput id='meuInput' type="text" placeholder="Digite Algo" labelText='task' title='sdasd'/>
                     </div>
                     <div className="formRow">
                         <p>Lorem ipsum dolor sit amet.</p>
                     </div>
                     <div className="formRow">
-                        <p>Ciclos</p>
-                        <p>0 0 0 0 0 0 0</p>
+                        <Cycles />
                     </div>
                     <div className="formRow">
-                        <button>Enviar</button>
+                        <DefaultButton icon={<PlayCircleIcon />} color='green'/>
+                        <DefaultButton icon={<StopCircleIcon />} color='red'/>
                     </div>
                 </form>
             </Container>
