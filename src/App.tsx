9ea092d9711b31d6eebac263a75112e6303fe4a1
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-12-21 22:49:49 -0300

    feat: iniciar implementação da função de troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,17 @@ import { Menu } from './components/Menu';
 import { Cycles } from './components/Cycles';
 import { Footer } from './components/Footer';
 import { PlayCircleIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/Defaultinput';
 import { DefaultButton } from './components/DefaultButton';
 
-// React Hooks
-import { useState } from 'react';
-
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-    // UseState: Quando o valor mudar, quero que todos os componentes que usam esse valor sejam atualizados automaticamente.
-    // No React, o useState retorna um array com 2 posições: [valorAtual, funçãoParaAtualizarOValor]
-    // Não podemos modificar o valor diretamente, precisamos usar a função para atualizar o valor.
-    const [numero, setNumero] = useState(0); // Desestruturação de array
-
-    function handleClick() {
-        // É uma boa prática usar uma função e não uma atribuição direta
-        setNumero(prevState => prevState + 1);
-    }
-
     return (
         <>
-            <Heading>
-                Número: <span>{numero}</span>
-            </Heading>
-            <button onClick={handleClick}>Aumentar</button>
             <Container>
                 <Logo />
             </Container>
@@ -48,7 +30,7 @@ export function App() {
                             id='meuInput'
                             type='text'
                             placeholder='Digite Algo'
-                            labelText={numero.toString()}
+                            labelText='task'
                             title='sdasd'
                         />
                     </div>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,25 +1,55 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from 'lucide-react';
-
+import { useState } from 'react';
 import style from './style.module.css';
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+
+type AvaliableThemes = 'dark' | 'light';
 
 export function Menu() {
+    const [theme, setTheme] = useState<AvaliableThemes>('dark');
+
+    function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        event.preventDefault(); // Não seguir o link
+        console.log('Clicado', Date.now());
+    }
+
     return (
         <>
+            <h1>{theme}</h1>
             <nav className={style.menu}>
-                <a className={style.menuLink} href="#">
+                <a
+                    className={style.menuLink}
+                    aria-label='Home'
+                    title='Home'
+                    href='#'
+                >
                     <HouseIcon />
                 </a>
-                <a className={style.menuLink} href="#">
+                <a
+                    className={style.menuLink}
+                    aria-label='Histórico'
+                    title='Histórico'
+                    href='#'
+                >
                     <HistoryIcon />
                 </a>
-                <a className={style.menuLink} href="#">
+                <a
+                    className={style.menuLink}
+                    aria-label='Configurações'
+                    title='Configurações'
+                    href='#'
+                >
                     <SettingsIcon />
                 </a>
-                <a className={style.menuLink} href="#">
+                <a
+                    className={style.menuLink}
+                    aria-label='Tema'
+                    title='Tema'
+                    href='#'
+                    onClick={handleThemeChange}
+                >
                     <SunIcon />
                 </a>
             </nav>
-            
         </>
     );
 }
