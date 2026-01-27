2a708013bba2d98dc6a5b4c270638e2250b49a02
Gabriel de Lima Sousa <gablimax@hotmail.com>
2026-01-27 19:06:33 -0300

    feat: Adicionar áudio de conclusão de task

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { TaskContext } from './TaskContext';
 import { initialTaskState } from './initialTaskState';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
 import { TaskActionTypes } from './taskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
     children: React.ReactNode;
@@ -11,34 +12,44 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
     const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+    const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
     const worker = TimerWorkerManager.getInstance();
 
-    worker.onmessage(e => {
-        const countDownSeconds = e.data;
-        console.log(countDownSeconds);
+    useEffect(() => {
+        worker.onmessage((e: MessageEvent) => {
+            const countDownSeconds = e.data;
 
-        if (countDownSeconds <= 0) {
-            dispatch({
-                type: TaskActionTypes.COMPLETE_TASK,
-            });
-            worker.terminate();
-        } else {
-            dispatch({
-                type: TaskActionTypes.COUNT_DOWN,
-                payload: { secondsRemaining: countDownSeconds },
-            });
-        }
-    });
+            if (countDownSeconds <= 0) {
+                if (playBeepRef.current) {
+                    playBeepRef.current();
+                    playBeepRef.current = null;
+                }
+                dispatch({ type: TaskActionTypes.COMPLETE_TASK });
+                worker.terminate();
+            } else {
+                dispatch({
+                    type: TaskActionTypes.COUNT_DOWN,
+                    payload: { secondsRemaining: countDownSeconds },
+                });
+            }
+        });
+    }, [worker]);
 
     useEffect(() => {
-        console.log(state);
         if (!state.activeTask) {
-            console.log('Worker terminado por falta de activeTask');
             worker.terminate();
         }
         worker.postMessage(state);
     }, [state, worker]);
+
+    useEffect(() => {
+        if (state.activeTask && playBeepRef.current === null) {
+            playBeepRef.current = loadBeep();
+        } else {
+            playBeepRef.current = null;
+        }
+    }, [state.activeTask]);
 
     return (
         <TaskContext.Provider value={{ state, dispatch }}>
